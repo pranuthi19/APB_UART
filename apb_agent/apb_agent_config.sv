af9e5061b5f3c291a263e0ecbc4278ab617e5a53
pranuthi19 <140632967+pranuthi19@users.noreply.github.com>
2026-07-02 20:47:09 +0530

    Create apb_agent_config.sv

---
+++ apb_agent/apb_agent_config.sv
@@ -0,0 +1,14 @@
+class apb_agent_config extends uvm_object;
+
+  `uvm_object_utils(apb_agent_config)
+
+  virtual apb_if vif;
+  uvm_active_passive_enum is_active;
+
+  extern function new(string name = "apb_agent_config");
+
+endclass : apb_agent_config
+
+function apb_agent_config::new(string name = "apb_agent_config");
+  super.new(name);
+endfunction : new
